fca9d0336eec4c46c430370946474f67c94f6eb9
GalloSimone <gallosimonecz@gmail.com>
2024-12-02 15:50:37 +0100

    add components

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import *as bootstrap from "bootstrap"
+
 import './index.css'
 import App from './App.jsx'
-import "bootstrap/dist/css/bootstrap.min.css"
-import *as boostrap from "bootstrap"
 
 
 createRoot(document.getElementById('root')).render(
